b54f939ec94e4fe6ac9c9a5b5ef4cf5fc3abd450
Broad1 <broad01.fivem@gmail.com>
2023-05-26 18:16:15 -0500

    Added Branding Section
    Added New Branding Section That Goes Into your server console. The Server.lua file. Updated Version to 1.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 name "BlipsAnywhere"
 Description "Place Blips Anywhere Using Configurable Locations"
 author 'Broad1'
-version "1.0.0"
+version "1.0.1"
 -- DO NOT TOUCH ^^ For Author Only
 
 -- Define the FX Server version and game type
@@ -10,3 +10,5 @@ fx_version "cerulean"
 
 -- Runs The Client Sided Scripts
 client_script "main.lua"
+
+server_script "server.lua"
---
+++ server.lua
@@ -0,0 +1,11 @@
+local brand =
+[[ 
+    
+         BlipsAnywhere
+Created By: ^5Broad Development LLC.
+           ^05/26/23
+
+]]
+
+
+print ( brand ) 
