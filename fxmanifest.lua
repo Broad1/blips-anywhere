31beda92a0d304108923b1e35798db2cb97d8108
Broad1 <broad01.fivem@gmail.com>
2023-05-26 17:47:03 -0500

    V1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+name "BlipsAnywhere"
+Description "Place Blips Anywhere Using Configurable Locations"
+author 'Broad1'
+version "1.0.0"
+-- DO NOT TOUCH ^^ For Author Only
+
+-- Define the FX Server version and game type
+game "gta5"
+fx_version "cerulean"
+
+-- Runs The Client Sided Scripts
+client_script "main.lua"
---
+++ main.lua
@@ -0,0 +1,23 @@
+local blips = {
+  -- {title="", colour=, id=, x=, y=, z=},
+
+  {name="Mechanic", color=5, id=446, x = -347.291, y = -133.370, z = 38.009},
+  {name="Pacific Bank", colour=5, id=108, x = 260.130, y = 204.308, z = 109.287},
+}
+
+
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z) -- Adds the blip at location determined inside each line of blips.lua
+      SetBlipSprite(info.blip, info.id) -- Sets the displayed sprite for a specific blips. Determined in the blips.lua
+      SetBlipDisplay(info.blip, 3)
+      -- Blip Display Behavior: 0 = Never Shows Up, 2 = Shows Up On Both Mini and Main Map , 3 = Shows up on main map (selectable), 5 = Shows up on minimap only , 8 = Shows on both maps
+      SetBlipScale(info.blip, 0.6) -- Sets The Blip Scale from 0.1 to 1.0
+      SetBlipColour(info.blip, info.color) -- Sets The Color Based on the Color ID Selected inside blips.lua
+      SetBlipAsShortRange(info.blip, true) -- Sets whether or not the specified blip should only be displayed when nearby, or on the minimap.
+	  BeginTextCommandSetBlipName("STRING") -- "STRING" entails that whatever is changed in the blips.lua file under the NAME section is what will show
+      AddTextComponentString(info.name)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)
